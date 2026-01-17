21db9f26279a4abed34b67731a661befea88230f
sahinkaraoglan <sahinnkaraoglann@gmail.com>
2026-01-18 02:09:34 +0300

    Current

--- next/app/components/auth/RegisterClient.tsx
+++ next/app/components/auth/RegisterClient.tsx
@@ -7,8 +7,13 @@ import Button from "../general/Button"
 import Heading from "../general/Heading"
 import Input from "../general/Input"
 import Link from "next/link"
+import axios from "axios"
+import toast from "react-hot-toast"
+import { signIn } from "next-auth/react"
+import { useRouter } from "next/navigation"
 
 const RegisterClient = () => {
+  const router = useRouter();
   const {
         register,
         handleSubmit,
@@ -17,8 +22,25 @@ const RegisterClient = () => {
       } = useForm<FieldValues>()
 
       const onSubmit: SubmitHandler<FieldValues> = (data) => {
-        console.log(data)
-      }
+        axios.post('/api/register', data).then(() => {
+          toast.success('Kullanıcı Olusturuldu...')
+           signIn('credentials', {
+                email: data.email,
+                password: data.password,
+                redirect: false
+            }).then((callback) => {
+                if(callback?.ok){
+                    router.push('/cart')
+                    router.refresh();
+                    toast.success('Login İşlemi Basarılı...')
+                }
+                if(callback?.error){
+                    toast.error(callback.error)
+                }
+        })
+      })
+    }
+
   return (
     <AuthContainer>
 
