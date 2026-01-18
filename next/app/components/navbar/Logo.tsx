5e9e06d71e5a483748bb5e6e985f7a1adb1db18c
sahinkaraoglan <sahinnkaraoglann@gmail.com>
2026-01-18 16:13:31 +0300

    Current

--- next/app/components/navbar/Logo.tsx
+++ next/app/components/navbar/Logo.tsx
@@ -1,7 +1,11 @@
 "use client"
+
+import { useRouter } from "next/navigation"
+
 const Logo = () => {
+  const router = useRouter()
   return (
-    <div className="bg-orange-700 px-2 py-1 rounded-md text-lg md:text-2xl cursor-pointer">
+    <div onClick={() => router.push('/')} className="bg-orange-700 px-2 py-1 rounded-md text-lg md:text-2xl cursor-pointer">
       Burada 
       <span className="text-sm">.com</span>
     </div>
--- next/app/components/navbar/Navbar.tsx
+++ next/app/components/navbar/Navbar.tsx
@@ -1,17 +1,19 @@
+import { getCurrentUser } from "@/app/actions/getCurrentUser"
 import CardCount from "./CardCount"
 import HamburgerMenu from "./HamburgerMenu"
 import Logo from "./Logo"
 import Search from "./Search"
 import User from "./User"
 
-const Navbar = () => {
+const Navbar = async () => {
+    const currentUser = await getCurrentUser();
   return (
-    <div className="flex items-center justify-between gap-3 md:gap-10 px-3 md:px-10 h-16 bg-orange-600 text-slate-100">
+    <div className="flex items-center justify-between gap-3 md:gap-10 px-3 md:px:10 h-16 bg-orange-600 text-slate-100">
         <Logo/>
         <Search/>
         <CardCount/>
-        <User/>
-        <HamburgerMenu/>
+        <User currentUser={currentUser}/>
+        <HamburgerMenu/> 
     </div>
   )
 }
