206973f8469951535a8d61dc227c7b2cfe021814
sahinkaraoglan <sahinnkaraoglann@gmail.com>
2026-01-19 00:18:28 +0300

    Current

--- next/app/admin/create/page.tsx
+++ next/app/admin/create/page.tsx
@@ -1,7 +1,20 @@
+import { getCurrentUser } from "@/app/actions/getCurrentUser"
+import WarningText from "@/app/components/WarningText"
+import CreateForm from "@/app/components/admin/CreateForm"
+import AuthContainer from "@/app/components/containers/AuthContainer"
 
-const CreateProduct = () => {
+const CreateProduct = async() => {
+  const currentUser = await getCurrentUser()
+
+  if(!currentUser || currentUser.role !== "ADMIN"){
+    return (
+      <WarningText text="Buraya Girişin Yasaklı !!!"/>
+    )
+  }
   return (
-    <div>CreateProduct</div>
+    <AuthContainer>
+       <CreateForm/>
+    </AuthContainer>
   )
 }
 
---
+++ next/app/components/WarningText.tsx
@@ -0,0 +1,7 @@
+const WarningText = ({text}: {text: string}) => {
+  return (
+    <div className="text-slate-600 text-lg h-screen w-full flex items-center justify-center">{text}</div>
+  )
+}
+
+export default WarningText
